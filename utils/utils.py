17e64cd41c6f9d5abc6c7eedb061079cbf0e2002
Feng Gu <fenggu2000@gmail.com>
2022-11-11 12:41:03 -0500

    analysis of the data

--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,15 @@
 from collections import Counter
+from typing import List
 
-def trim(token):
+# remove parentheses
+def trim(token: str) -> str:
     while token.startswith('('):
         token = token[1:]
     while token.endswith(')'):
         token = token[:-1]
     return token
 
-def compute_accuracy(reference, translation):
+def compute_accuracy(reference: str, translation: str) -> float:
     translated_tokens = translation.split(' ')
     reference_tokens = reference.split(' ')
     trimmed_translated_tokens = [trim(token) for token in translated_tokens]
@@ -17,21 +19,23 @@ def compute_accuracy(reference, translation):
     # recall = correct / reference-length
     # f = p * q * 2 / (p + q)
     correct = list((Counter(trimmed_reference_tokens) & Counter(trimmed_translated_tokens)).elements())
-    precision = len(correct) / len(trimmed_translated_tokens)
-    recall = len(correct) / len(trimmed_reference_tokens)
+    overlap = len(correct)
+    # return if denom is 0
+    if overlap == 0:
+        return 0
+    precision = overlap / len(trimmed_translated_tokens)
+    recall = overlap / len(trimmed_reference_tokens)
     f = precision * recall * 2 / (precision + recall)
-    return f
+    return round(f, 3)
 
-def read_file():
-    count = 1
+def read_file() -> List[str]:
     with open('data/generated_daide_clean.txt', 'r') as f:
         lines = f.read()
         entries = lines.split('\n\n')
     
     for entry in entries:
         english, reference, translation = entry.split('\n')
-        print(count)
-        count += 1
+        print(compute_accuracy(reference, translation))
     return lines
 
 if __name__ == '__main__':
