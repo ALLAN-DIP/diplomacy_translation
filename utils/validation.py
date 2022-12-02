badc4d567ccd62c933e74d5e73b4b1410ea3c6e8
SiddarGu <fenggu2000@gmail.com>
2022-12-01 19:27:02 -0500

    1/3 annotated data

--- utils/validation.py
+++ utils/validation.py
@@ -1,31 +1,25 @@
 from daidepp import create_daide_grammar, daide_visitor
 
-grammar = create_daide_grammar(level=130)
-message = 'PRP (XDO ((TUR AMY SER) MTO TRI))'
+grammar = create_daide_grammar(level=130, string_type='all')
+message = 'PRP (XDO ((ENG FLT SKA) MTO DEN))'
 parse_tree = grammar.parse(message)
 output = daide_visitor.visit(parse_tree)
 print(output)
 
 '''
-Not validated annotations:
-PRP ((ITA SUP ((FRA AMY RUH) MTO MUN)) (FRA SUP (ITA AMY TYR)))
-missing AND
+Most common errors:
+- Missing AND to connect two conditions
+    Example: ((TUR FLT SMY) BLD) ((TUR AMY CON) BLD) -> AND (XDO ((TUR FLT SMY) BLD)) (XDO ((TUR AMY CON) BLD))
+- Missing XDO wrapper for orders
+    Example: PRP ((RUS AMY TRI) MTO VIE) -> XDO ((RUS AMY TRI) MTO VIE)
+- *Missing VSS for ALY arrangements
+    Example: PRP (ALY (FRA RUS)) -> PRP (ALY (FRA RUS) VSS (ENG))
+- *Using CTY instead of unit for SUP orders
+    Example: RUS SUP MUN -> (RUS AMY BOH) SUP MUN
+- Some orders are missing CTY
+- daide seems to have issue recognizing SCD (opened an issue on GitHub)
+- Mix usage of REJ (reply, which takes a message as an argument) and NOT (arrangement, which is an argument of AND)
+- daide++ cannot handle two PRP in one message (using AND won't work)
 
-PRP (PRP (NOT (RUS SUP MUN)))
-double PRP
-
-PRP (ALY (GER AUS))
-arrangement = ALY (power power ...) VSS (power power ...)
-
-PRP (GER SUP ((AUS AMY VIE) MTO TYR))
-( unit_with_location ) SUP ( unit_with_location )
-
-YES (ALY (GER FRA) VSS (ENG))
-missing PRP: YES (PRP (ALY (GER FRA) VSS (ENG)))
-
-NOT (ALY (GER) VSS (ENG))
-not considered as a message in daide++
-
-PRP ((TUR AMY SER) MTO TRI)
-not an arrangement: PRP (XDO ((TUR AMY SER) MTO TRI))
+* Inferring unit/opponent from message is not guaranteed.
 '''
