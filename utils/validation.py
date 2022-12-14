65bf1c7ef380736476fdf54873f8a16c93a817a7
SiddarGu <fenggu2000@gmail.com>
2022-12-14 15:56:39 -0500

    all but 2 annotations

--- utils/validation.py
+++ utils/validation.py
@@ -1,7 +1,7 @@
 from daidepp import create_daide_grammar, daide_visitor
 
 grammar = create_daide_grammar(level=130, string_type='all')
-message = 'PRP (DMZ (FRA ITA) (PIE LYO WES TYS))'
+message = 'YES (PRP (AND (ALY (ENG FRA) VSS (GER)) (DMZ (ENG FRA) (ECH))))'
 parse_tree = grammar.parse(message)
 output = daide_visitor.visit(parse_tree)
 print(output)
