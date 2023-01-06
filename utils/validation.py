119ced3fbd26d5c74202a3c2c052dc5dd6f0384c
Feng Gu <fenggu2000@gmail.com>
2023-01-06 17:23:59 -0500

    english translation

--- utils/validation.py
+++ utils/validation.py
@@ -1,7 +1,26 @@
-from daidepp import create_daide_grammar, daide_visitor
+from translation.translation import toEnglish
+import json
+from time import sleep
 
-daide = 'YES (PRP (ALY (ITA AUS) VSS (TUR)))'
-grammar = create_daide_grammar(level=130, string_type='all')
-parse_tree = grammar.parse(daide)
-output = daide_visitor.visit(parse_tree)
-print(output)
+all_generated = []
+count = 0
+total = 312
+
+with open ('data/annotated_daide.json', 'r') as f:
+    data = json.load(f)
+
+    for entry in data:
+        msg = entry['msg']
+        daide = entry['daide']
+        count += 1
+        
+        generated = toEnglish(daide)
+        all_generated.append({'msg': msg, 'daide': daide, 'translation': generated})
+        print(f'{count}/{total}')
+        sleep(5)
+        
+    f.close()
+
+with open ('data/daide_to_english.json', 'x') as f:
+    json.dump(all_generated, f, indent=4)
+    f.close()
